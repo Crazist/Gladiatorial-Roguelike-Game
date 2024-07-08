2092ff1f4199e1709bad4143b881b2c19d35cb7b
Crazist <86979691+Crazist@users.noreply.github.com>
2024-07-08 14:44:51 +0300

    SceneContext

---
+++ Gladiatorial-Roguelike/Assets/Scripts/Infrastructure/MainSceneInstaler.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using Zenject;
+
+public class MainSceneInstaler : MonoInstaller
+{
+ //   Container.Bind<BattlePassRewardSystem>().AsSingle().NonLazy();
+           
+  //  Container.BindInstance(_chestReward).AsSingle();
+}
