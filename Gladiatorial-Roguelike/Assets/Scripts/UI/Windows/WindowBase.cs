eb55ff4d4f0fdab6f6e97b52b30097a9b7f82dfc
Crazist <86979691+Crazist@users.noreply.github.com>
2024-07-10 02:54:28 +0300

    Reactive UI

--- Gladiatorial-Roguelike/Assets/Scripts/UI/Windows/WindowBase.cs
+++ Gladiatorial-Roguelike/Assets/Scripts/UI/Windows/WindowBase.cs
@@ -7,12 +7,25 @@ namespace UI
     {
         [SerializeField] private Button _closeBtn;
 
-        private void Awake()
-        {
+        private void Awake() => 
             OnAwake();
+
+        private void Start()
+        {
+            Initialize();
+            SubscribeUpdates();
+        }
+
+        private void OnDestroy()
+        {
+            CleanUp();
         }
 
         protected virtual void OnAwake() =>
             _closeBtn?.onClick.AddListener(() => Destroy(gameObject));
+
+        protected virtual void Initialize(){}
+        protected virtual void SubscribeUpdates(){}
+        protected virtual void CleanUp(){}
     }
 }
