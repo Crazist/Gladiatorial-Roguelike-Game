daec962c37f871e0bd96f848aa5743f025956b6e
Crazist <86979691+Crazist@users.noreply.github.com>
2024-07-09 21:36:36 +0300

    Choose Deck Ui

---
+++ Gladiatorial-Roguelike/Assets/Scripts/UI/WindowBase.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public abstract class WindowBase : MonoBehaviour
+    {
+        [SerializeField] private Button _closeBtn;
+
+        private void Awake()
+        {
+            OnAwake();
+        }
+
+        protected virtual void OnAwake() =>
+            _closeBtn.onClick.AddListener(() => Destroy(gameObject));
+    }
+}
